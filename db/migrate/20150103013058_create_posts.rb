4aac140ce6e825f8a313f893f8ea47e3313bfac4
Katie <kab761@gmail.com>
2015-01-02 17:31:50 -0800

    made post and user models

---
+++ db/migrate/20150103013058_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.boolean :published
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
