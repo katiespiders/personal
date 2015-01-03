f7f0d20eebd74b2399a9047a0ab4c830ba0476cc
Katie <kab761@gmail.com>
2015-01-02 19:57:01 -0800

    typo

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :post do |f|
-    f.title { Faker::Lorem.sentence.titleize }
+    f.title { Faker::Lorem.sentence }
     f.content { Faker::Lorem.paragraphs.join("\n") }
     f.published_at { Faker::Time.backward(30) }
   end
