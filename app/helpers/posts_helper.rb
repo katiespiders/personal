8c3c0aa8fbb9355baeccf473982820c102be9bfd
Katie <kab761@gmail.com>
2015-01-04 15:58:28 -0800

    refactored posts index view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+
+  def preview(post)
+
+    html = "<h2>#{link_to post.title, post_path(post.id)} "
+    html += "<small>#{post.timestamp}</small></h2>"
+    html += "<article class='preview'>#{content_preview(post)}</article>"
+    html += "<article class='full'>#{post.content}</article>"
+    html.html_safe
+
+  end
+
+  private
+
+    def content_preview(post)
+      post.content.length < 500 ? post.content : /\A.{500}\S*/m.match(post.content).to_s + " ... "
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
+
+  def timestamp
+    format = '%-d %B %Y'
+    published? ? published_at.strftime(format) : updated_at.strftime(format)
+  end
 end
