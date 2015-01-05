c9e9cc0d1e3939d3275a76eb33d0cc8f6d68de7d
Katie <kab761@gmail.com>
2015-01-04 16:43:50 -0800

    refactoring

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
 
+  def self.sort
+    all.to_a.sort_by{ |post| post.sort_timestamp }.reverse
+  end
 
+  def sort_timestamp
+    published? ? published_at : updated_at
+  end
 end
