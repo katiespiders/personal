926c10d8df64b4f1c7093aa9ec85b97b72b62007
Katie <kab761@gmail.com>
2015-01-04 19:49:12 -0800

    initial post model tests

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,13 @@ require 'faker'
 FactoryGirl.define do
   factory :post do |f|
     f.title { Faker::Lorem.sentence }
-    f.content { Faker::Lorem.paragraphs.join("\n") }
-    f.published_at { Faker::Time.backward(30) }
+    f.content { Faker::Lorem.paragraphs(10).join("\n") }
+
+    trait :published do
+      published? true
+      published_at Faker::Time.backward(30)
+    end
   end
+
+  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
+  let (:post) { FactoryGirl.create(:post) }
+
   it 'has a valid factory' do
-    expect(FactoryGirl.create(:post)).to be_valid
+    expect(post).to be_valid
   end
 
   it 'must have a title' do
@@ -11,5 +13,58 @@ RSpec.describe Post, :type => :model do
 
   it 'must have content' do
     expect(FactoryGirl.build(:post, content: nil)).not_to be_valid
+  end
+
+  context 'Published post' do
+    let (:published_post) { FactoryGirl.create(:post, :published) }
+
+    it 'has published status and published_at timestamp' do
+      expect(published_post.published?).to eq true
+      expect(published_post.published_at).not_to eq nil
+    end
+
+    it 'uses published_at, not ActiveRecord timestamps, as timestamp' do
+      ts = published_post.timestamp
+
+      expect(ts).to eq published_post.published_at
+      expect(ts).not_to eq published_post.updated_at
+      expect(ts).not_to eq published_post.created_at
+    end
+  end
+
+  context 'Draft post' do
+    let (:draft) { FactoryGirl.create(:post) }
+
+    it 'is created with unpublished status' do
+      expect(post.published?).to eq false
+      expect(post.published_at).to eq nil
+    end
+
+    it 'uses ActiveRecord updated_at timestamp, not published_at, as timestamp' do
+      ts = draft.timestamp
+
+      expect(ts).to eq draft.updated_at
+      expect(ts).not_to eq draft.published_at
+    end
+  end
+
+  describe 'Class methods' do
+    posts = [
+      FactoryGirl.create(:post, :published, published_at: 1.day.ago, created_at: 4.days.ago, updated_at: 2.days.ago),
+      FactoryGirl.create(:post, created_at: 6.days.ago, updated_at: 2.days.ago),
+      FactoryGirl.create(:post, :published, published_at: 3.days.ago),
+      FactoryGirl.create(:post, :published, published_at: 4.days.ago),
+      FactoryGirl.create(:post, created_at: 5.days.ago, updated_at: 5.days.ago)
+    ]
+
+    let(:desc_posts) { posts }
+
+    it 'sorts unpublished (by updated_at) with published (published_at)' do
+      allow(Post).to receive(:all).and_return( desc_posts.shuffle)
+
+      expect(Post.sort_desc).to eq desc_posts
+      expect(Post.sort_asc).to eq desc_posts.reverse
+    end
+
   end
 end
